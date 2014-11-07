d109ce91ae53c161b9ea0ecab53a9e3ac200b282
root <root@www14422ui.sakura.ne.jp>
2014-11-08 07:29:04 +0900

    top

---
+++ content.php
@@ -0,0 +1,27 @@
+<section class="container">
+    <div class="row">
+      <div class="col-sm-3" id="menu" name="menu">
+      <ul class="nav nav-pills nav-stacked">
+      <li><a href=#>俺の上司を投稿</a></li>
+      <li><a href=#>俺の上司を辱め履歴</a></li>
+      </ul> 
+     </div>
+      <div class="col-sm-9" >
+      <img src=./image/top.jpg class=col-sm-6>
+      【俺の上司がこんなに可愛いわけがないとは！】<br>
+       簡単3ステップで上司が可愛い<br>
+      猫耳姿になるサイトです。<br>
+      <ol>
+　　　<li >1,上司画像を勝手に投稿</li>
+　　　<li >2,上司画像が猫耳上司へ</li>
+      <li >3,上司ラブ！</li>
+      </ol>
+
+    <form enctype="multipart/form-data" action="__URL__" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    上司で萌える:<input name="userfile" type="file" />
+    <input type="submit" value="上司をアップロード" />
+    </form>
+      </div>
+    </div>
+</section>
---
+++ footer.php
@@ -0,0 +1,4 @@
+<hr />
+<div class="text-center footer">
+  <p>---------------------- CopyRight シャチク野郎Aチーム.syachiku,ltd  2014 ----------------------</p>
+</div>
