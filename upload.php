93bd5493682c579dd70bc5b919e69b10c758cb08
root <root@www14422ui.sakura.ne.jp>
2014-11-08 09:17:56 +0900

    画像アップロード完了

--- upload.php
+++ upload.php
@@ -1,12 +1,9 @@
 <?php
 // 画像アップロードと移動
-$image_dir = '/var/www/html/files/';
+$image_dir = './files/';
 $file = $_FILES['upfile'];
 if (is_uploaded_file($file['tmp_name'])) {
     move_uploaded_file($file['tmp_name'], $image_dir . $file['name']);
+    header("Location: ". $image_dir . $file['name']);
 }
-echo $_FILE["input_name"]["error"];
-phpinfo();
-
-
 ?>
